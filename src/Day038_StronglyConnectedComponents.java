eb1799fec4eb0e2dad4c85bc35cf1fd8fbf910b6
suhebxsiddique <suhebxsiddique@gmail.com>
2024-05-22 16:00:00 +0530

    Add Day038_StronglyConnectedComponents.java

---
+++ src/Day038_StronglyConnectedComponents.java
@@ -0,0 +1,5 @@
+﻿public class Day038_StronglyConnectedComponents {
+    public static void main(String[] args) {
+        System.out.println("Day038_StronglyConnectedComponents - placeholder solution");
+    }
+}
